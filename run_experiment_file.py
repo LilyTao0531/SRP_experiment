646e91cdc3b166b4740123f01393ea65e7472148
Lily tao <bravolavier@gmail.com>
2022-08-29 17:40:16 -0400

    Removed the folder scheduling as it is unrelated to the experiment

--- run_experiment_file.py
+++ run_experiment_file.py
@@ -8,7 +8,7 @@ a = Experiment("/home/lily/srp_experiment/")
 # a.compare_re([5, 10, 20], ["cp", "m3"], time_lim = 600, time_increment = 0.01)
 # a.generate_instances([5], 20)
 
-a.check_feasibility([1000], ['cp'], 1)
+a.check_feasibility([5], ['cp'], 10)
 
 # a.compare_re([2000], ['cp', 'm3'], time_lim = 60, time_increment = 0.01)
 
